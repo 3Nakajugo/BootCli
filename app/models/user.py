8e7d97fa9e60f08b01b9d9eb6b9110f8f4bf2b2e
Frank <fatukunda@gmail.com>
2018-10-29 11:32:23 +0300

    search user

--- app/models/user.py
+++ app/models/user.py
@@ -3,5 +3,20 @@ class User:
         self.username = username
         self.password = password
         self.role = role
+    """ Save the created user to a list of users"""
+    def save_users(self,user):
+        USERS.append(user)
 
-USERS = {}
+USERS = []
+""" View a list of users"""
+def get_users():
+    return USERS
+
+@staticmethod
+def search_user(username):
+    for user in USERS:
+        for key in user:
+            if user[key] == username:
+                return user
+            else:
+                return username + ' was not found'
